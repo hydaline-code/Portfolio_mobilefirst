d9b1b033e5f2c1985e434cddc9de9f081da36aa4
hydaline-code <djougancharlen@gmail.com>
2023-06-30 10:21:02 +0100

    Linters file checks for js file

--- mobile.js
+++ mobile.js
@@ -3,53 +3,48 @@ const openbtn = document.querySelector('.open');
 const hamburger = document.querySelector('.menu i:nth-child(1)');
 const menu1 = document.querySelector('#menulist');
 const menu = document.querySelector('.menu-d-list');
-const items =document.querySelectorAll('.item > a');
+const items = document.querySelectorAll('.item > a');
 const headline = document.querySelector('.headline-d');
 const img = document.querySelector('.headline-img');
 const img1 = document.querySelector('.rmimg');
 const img2 = document.querySelector('.rmimg1');
-const img3 = document.querySelector('.addimg')
+const img3 = document.querySelector('.addimg');
 const first = document.querySelector('.line');
 const navmenu = document.querySelector('#menu-nav ul');
-const navmenu1 = document.querySelector('#menu-nav div');
 const home = document.querySelector('.headline');
-
-
 
 hamburger.addEventListener('click', () => {
   menu1.classList.toggle('menu-d-list');
-  navmenu.style.cssText='margin-top:9rem; text-align:left; line-height:300%; margin-right:calc(70% + 4rem); font-size:20px; height:310px; padding-top:5rem';
-  menu.style.listStyle='none';
-  menu.style.display='inline-block';
-  first.style.display='none'
-  closebtn.style.display='block';
-  openbtn.style.display='none';
-  headline.style.display='none';
+  navmenu.style.cssText = 'margin-top:9rem; text-align:left; line-height:300%; margin-right:calc(70% + 4rem); font-size:20px; height:310px; padding-top:5rem';
+  menu.style.listStyle = 'none';
+  menu.style.display = 'inline-block';
+  first.style.display = 'none';
+  closebtn.style.display = 'block';
+  openbtn.style.display = 'none';
+  headline.style.display = 'none';
   img1.style.display = 'none';
-  img3.style.display='block';
+  img3.style.display = 'block';
   img.style.display = 'none';
-  img2.style.display='block';
-  items.forEach(items => {
+  img2.style.display = 'block';
+  items.forEach((items) => {
     items.style.cssText = 'color: inherit; text-decoration: none; text-align:center';
   });
-  
-})
+});
 
 function menuclose() {
-  closebtn.style.display='none';
+  closebtn.style.display = 'none';
   home.style.display = 'block';
   hamburger.style.display = 'block';
-  menu.style.display='none';
-  headline.style.display='block';
+  menu.style.display = 'none';
+  headline.style.display = 'block';
   img1.style.display = 'block';
-  img3.style.display='none';
+  img3.style.display = 'none';
   img.style.display = 'block';
-  img2.style.display='none';
+  img2.style.display = 'none';
 }
 
 closebtn.addEventListener('click', menuclose);
 
-items.forEach(items => {
+items.forEach((items) => {
   items.addEventListener('click', menuclose);
 });
-
