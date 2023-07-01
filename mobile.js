836e19f6300d546b3c3c7f11733ec4e6809641bc
hydaline-code <djougancharlen@gmail.com>
2023-07-01 10:48:17 +0100

    Update js file for popup window

--- mobile.js
+++ mobile.js
@@ -215,4 +215,140 @@ projects_dynamic.forEach((project) => {
     tagLi.textContent = tech.name;
   });
 
+
+
+const popupwindow = document.querySelector('.popup_window');
+
+btn.addEventListener('click', () => {
+  popupwindow.style.display = 'block';
+  const popheading = document.querySelector('.update_heading');
+  popheading.textContent = project.name;
+
+  const modalimage = document.querySelector('.modall-img');
+  const imgmodal = document.createElement('img');
+  modalimage.appendChild(imgmodal);
+  imgmodal.classList.add('modal-img');
+  imgmodal.src = 'images/Group 109.png';
+
+  const modalpara = document.querySelector('.modal-p');
+  const para = document.createElement('p');
+  modalpara.appendChild(para);
+  para.textContent = project.description;
+
+  project.technologies.forEach((tech) => {
+    const modallang = document.querySelector('.modal-lang ul');
+    const langli = document.createElement('li');
+    modallang.appendChild(langli);
+    const Lia = document.createElement('a');
+    langli.appendChild(Lia);
+    Lia.textContent = tech.name;
+  });
 });
+
+const close2 = document.querySelector('.popup-close');
+const modallang = document.querySelector('.modal-lang ul');
+close2.addEventListener('click', () => {
+  popupwindow.style.display = 'none';
+
+  const removeLi = document.querySelector('.modal-lang ul li ');
+  if (removeLi !== null) modallang.removeChild(removeLi);
+
+  const modalimage = document.querySelector('.modall-img');
+  const removeimg = document.querySelector('.modall-img img');
+  if (removeimg !== null) modalimage.removeChild(removeimg);
+});
+
+});
+
+
+const desktoptech = ['HTML/CSS', ' Ruby on Rails', 'JavaScript'];
+const project2 = document.querySelector('.project-des-d');
+const project22 = document.querySelector('.project-des-d1');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+project2.appendChild(btn1);
+project22.appendChild(btn2);
+btn1.classList.add('btn-projects');
+btn2.classList.add('btn-projects');
+btn1.textContent = 'See this project  \u2192';
+btn2.textContent = 'See this project  \u2192';
+
+const modalimage = document.querySelector('.modall-img');
+const removeimg = document.querySelector('.modall-img img');
+if (removeimg !== null) modalimage.removeChild(removeimg);
+
+
+function popdesktop() {
+  const popupwindow = document.querySelector('.popup_window');
+
+  popupwindow.style.display = 'block';
+  const popheading = document.querySelector('.update_heading');
+  popheading.textContent = 'Project-desktop name goes here';
+
+  const modalimage = document.querySelector('.modall-img');
+  const imgmodal = document.createElement('img');
+  modalimage.appendChild(imgmodal);
+  imgmodal.classList.add('modal-img');
+  imgmodal.src = 'images/Group 105.png';
+
+  const modalpara = document.querySelector('.modal-p');
+  const para = document.createElement('p');
+  modalpara.appendChild(para);
+  const para1 = document.createElement('p');
+  para.classList.add('para-desk');
+  para1.classList.add('para-desk1');
+  modalpara.appendChild(para1);
+  para.textContent = 'Lorem ipsum dolor sit amet,  aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis';
+  para1.textContent = 'Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <br>labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi';
+
+  const divdesktop = document.querySelector('.divdestop');
+  const smallbtn = document.createElement('button');
+  const smallbtn1 = document.createElement('button');
+  divdesktop.appendChild(smallbtn);
+  divdesktop.appendChild(smallbtn1);
+  smallbtn.classList.add('arrow');
+  smallbtn1.classList.add('arrow1');
+  smallbtn.textContent = ' \u2190 Previous project';
+  smallbtn1.textContent = 'Next project  \u2192';
+
+  for (let i = 0; i < desktoptech.length; i += 1) {
+    const modallang = document.querySelector('.modal-lang ul');
+    const langli = document.createElement('li');
+    modallang.appendChild(langli);
+    const Lia = document.createElement('a');
+    langli.appendChild(Lia);
+    Lia.textContent = desktoptech[i];
+  }
+}
+
+
+
+btn1.addEventListener('click', popdesktop);
+btn2.addEventListener('click', popdesktop);
+
+const close2 = document.querySelector('.popup-close');
+// const modal-lang = document.querySelector('.modal-lang ul');
+
+close2.addEventListener('click', () => {
+  const popupwindow = document.querySelector('.popup_window');
+  popupwindow.style.display = 'none';
+
+  const modalpara = document.querySelector('.modal-p ');
+  const removep = document.querySelector('.para-desk');
+  if (removep !== null) modalpara.removeChild(removep);
+  const removep1 = document.querySelector('.para-desk1');
+  if (removep1 !== null) modalpara.removeChild(removep1);
+
+  const divdesktop = document.querySelector('.divdestop');
+  const removearr = document.querySelector('.arrow1');
+  if (removearr !== null) {
+    divdesktop.removeChild(removearr);
+  }
+  const removearr1 = document.querySelector('.arrow');
+  if (removearr1 !== null) {
+    divdesktop.removeChild(removearr1);
+  }
+
+
+});
+
